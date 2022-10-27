970994165a931181b91e8e2cc6e74bda883ffe65
Eden <levine5blue@gmail.com>
2022-10-27 22:12:12 +0800

    [ADD] ARC project.

---
+++ CyclicalAssets/CyclicalAssets/Vault.swift
@@ -0,0 +1,33 @@
+//
+//  Vault.swift
+//  CyclicalAssets
+//
+//  Created by LZhenHong on 2022/10/27.
+//
+
+import Foundation
+
+class Vault {
+    let number: Int
+    
+    private(set) var assets: [Asset] = []
+    
+    init(number: Int) {
+        self.number = number
+    }
+    
+    deinit {
+        print("\(self) is being deallocated.")
+    }
+    
+    func store(_ asset: Asset) {
+        asset.container = self
+        assets.append(asset)
+    }
+}
+
+extension Vault: CustomStringConvertible {
+    var description: String {
+        return "Vault(\(number))"
+    }
+}

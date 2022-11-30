cee6591e4aaab921329304b5a3769d4597d406e2
Eden <levine5blue@gmail.com>
2022-11-30 16:48:24 +0800

    [UPDATE] Ref vs value type.

--- Chapter25.playground/Contents.swift
+++ Chapter25.playground/Contents.swift
@@ -42,3 +42,86 @@ var wallBlue = azure
 azure
 wallBlue.darken()
 azure
+
+struct PaintingPlan {
+    var accent = Color.white
+    private var bucket = Bucket(color: .green)
+    
+    var bucketColor: Color {
+        set {
+            if isKnownUniquelyReferenced(&bucket) {
+                bucket.color = bucketColor
+            } else {
+                bucket = Bucket(color: newValue)
+            }
+        }
+        get {
+            bucket.color
+        }
+    }
+}
+
+private class Pixels {
+    let storageBuffer: UnsafeMutableBufferPointer<UInt8>
+    
+    init(size: Int, value: UInt8) {
+        let p = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
+        storageBuffer = UnsafeMutableBufferPointer(start: p, count: size)
+        storageBuffer.initialize(from: repeatElement(value, count: size))
+    }
+    
+    init(pixels: Pixels) {
+        let otherStorage = pixels.storageBuffer
+        let p = UnsafeMutablePointer<UInt8>.allocate(capacity: otherStorage.count)
+        storageBuffer = UnsafeMutableBufferPointer<UInt8>(start: p, count: otherStorage.count)
+        storageBuffer.initialize(from: otherStorage)
+    }
+    
+    subscript(offset: Int) -> UInt8 {
+        set {
+            storageBuffer[offset] = newValue
+        }
+        get {
+            storageBuffer[offset]
+        }
+    }
+    
+    deinit {
+        storageBuffer.baseAddress!.deallocate()
+    }
+}
+
+struct Image {
+    private(set) var width: Int
+    private(set) var height: Int
+    private var pixels: Pixels
+    private var mutatingPixels: Pixels {
+        mutating get {
+            if !isKnownUniquelyReferenced(&pixels) {
+                pixels = Pixels(pixels: pixels)
+            }
+            return pixels
+        }
+    }
+    
+    init(width: Int, height: Int, value: UInt8) {
+        self.width = width
+        self.height = height
+        self.pixels = Pixels(size: width * height, value: value)
+    }
+    
+    subscript(x: Int, y: Int) -> UInt8 {
+        set {
+            mutatingPixels[y * width + x] = newValue
+        }
+        get {
+            return pixels[y * width + x]
+        }
+    }
+    
+    mutating func clear(with value: UInt8) {
+        for (i, _) in self.mutatingPixels.storageBuffer.enumerated() {
+            self.mutatingPixels.storageBuffer[i] = value
+        }
+    }
+}
